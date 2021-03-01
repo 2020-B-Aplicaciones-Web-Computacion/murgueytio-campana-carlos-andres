a6710cc4f7a43b722b12295e89ff5b078143549f
Carlos Murgueytio <carlos.murgueytio@epn.edu.ec>
2021-03-01 10:54:51 -0500

    commit clase 10

--- clases/src/app.controller.ts
+++ clases/src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get} from '@nestjs/common';
+import {BadRequestException, Controller, ForbiddenException, Get, Param, Query, Req, Session} from '@nestjs/common';
 import {AppService} from './app.service';
 
 @Controller()
@@ -10,8 +10,65 @@ export class AppController {
     getHello(): string {
         return this.appService.getHello();
     }
-}
 
+    @Get('login')
+    login(
+        @Session() session,
+        @Query() parametrosConsulta,
+    ): string {
+        if (parametrosConsulta.nombre && parametrosConsulta.apellido) {
+            session.usuario = {
+                nombre: parametrosConsulta.nombre,
+                apellido: parametrosConsulta.apellido
+            }
+            if (parametrosConsulta.apellido === 'Murgueytio') {
+                session.usuario.esAdministrador = true;
+            }
+            return 'Se logeo el usuario';
+        } else {
+            throw new BadRequestException('NO ENVIA NOMBRE Y APELLIDO'); //400
+        }
+
+    }
+
+    @Get('quien-soy')
+    quienSoy(
+        @Session() session,
+    ): string {
+        if (session.usuario) {
+            return session.usuario.nombre + ' ' + session.usuario.apellido;
+        } else {
+            return 'No te has logeado';
+        }
+    }
+
+    @Get('logout')
+    logout(
+        @Session() session,
+        @Req() request,
+    ): string {
+        session.usuario = undefined;
+        request.session.destroy();
+        return 'Gracias por visitarnos';
+    }
+
+    @Get('protegido')
+    protegido(
+        @Session() session,
+    ): string {
+        if (session.usuario) {
+            if (session.usuario.esAdministrador) {
+                return 'CONTENIDO SUPER OCULTO';
+            } else {
+                throw new ForbiddenException('No tienes rol Admin');
+            }
+
+        } else {
+            throw new ForbiddenException('No tienes rol Admin');
+        }
+
+    }
+}
 
 
 // Clases - TYPESCRIPT
--- clases/src/main.ts
+++ clases/src/main.ts
@@ -1,9 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import * as cookieParser from "cookie-parser";
+//import helmet from "helmet";
+const helmet = require("helmet");
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+var fileStoreOptions = {};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.use(session({
+        store: new FileStore(fileStoreOptions), //indica donde se van a guardar los datos de la sesion
+        secret: 'keyboard cat',
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            expires: new Date(Date.now() + (30 * 8600 * 100)) //
+        }
+    }));
+    app.use(cookieParser());
+    app.use(helmet());
+    await app.listen(3000);
 }
+
 bootstrap();
